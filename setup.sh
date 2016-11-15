223816a1041a45f8a2093faccd7f8b540b5fa7fc
james@jamespamplin.com <james@jamespamplin.com>
2016-11-15 15:03:41 +0000

    Use https repo url for better proxy compatibility

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo "Setting up the user environment..."
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
 # Install homeshick, checkout dotfiles
-git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 homeshick clone -b jamespamplin/dotfiles
 homeshick link -b dotfiles
