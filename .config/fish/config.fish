b118b6e9592cbd9ce96dcead54ca80119156a0b6
James Pamplin <james@jamespamplin.com>
2023-05-26 21:34:12 +0200

    feat: 🎸 1password cli plugins + aws aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,3 +12,19 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# 1password cli plugins
+if test -e $HOME/.config/op/plugins.sh
+  source $HOME/.config/op/plugins.sh
+end
+
+# aws session functions
+function aws-production
+  set -l env (aws configure export-credentials --profile production --format env)
+  eval $env
+end
+
+function aws-staging
+  set -l env (aws configure export-credentials --profile staging --format env)
+  eval $env
+end
